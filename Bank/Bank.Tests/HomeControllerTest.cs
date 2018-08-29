20d83c4724c39cbedf558600775340994cac6e1c
Benedict Lee <belee@A.local>
2018-08-29 17:26:05 +0800

    added unit test for home controller

--- Bank/Bank.Tests/HomeControllerTest.cs
+++ Bank/Bank.Tests/HomeControllerTest.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.Security.Principal;
+using System.Security.Claims;
 
 namespace Bank.Tests
 {
@@ -14,41 +15,242 @@ namespace Bank.Tests
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Deposit()
+        public void DepositNoValues()
         {
             var name = "testuser1";
             var balance = 1.1m;
             var accountNumber = 1;
 
-            var fakeHttpContext = new Mock<HttpContext>();
-            var fakeIdentity = new GenericIdentity(accountNumber.ToString());
-            var principal = new GenericPrincipal(fakeIdentity, null);
-
-            fakeHttpContext.Setup(t => t.User).Returns(principal);
-            var controllerContext = new Mock<ControllerContext>();
-            controllerContext.Setup(t => t.HttpContext).Returns(fakeHttpContext.Object);
-
-
+            
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, accountNumber.ToString())
+            }));
+            
             var mockUserDAL = new Mock<IUserDAL>();
             var mockTransactionDAL = new Mock<ITransactionDAL>();
             
             mockUserDAL.Setup(x => x.GetUserData(It.IsAny<long>())).Returns(new User {
-                LoginName = name,
-                Balance = balance
+                LoginName = name
             });
 
+            mockTransactionDAL.Setup(x => x.GetBalance(It.IsAny<long>())).Returns(balance);
+
             var _homeController = new HomeController(mockUserDAL.Object, mockTransactionDAL.Object);
-            _homeController.ControllerContext = controllerContext.Object;
+            _homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
 
             var result = _homeController.Deposit();
 
             var r = result as ViewResult;
 
             Assert.AreEqual(r.ViewData["LoginName"], name);
-            Assert.AreEqual(r.ViewData["AccountNumber"], accountNumber);
+            Assert.AreEqual(r.ViewData["AccountNumber"].ToString(), accountNumber.ToString());
             Assert.AreEqual(r.ViewData["Balance"], balance);
         }
 
-        
+        [TestMethod]
+        public void WithdrawNoValues()
+        {
+            var name = "testuser1";
+            var balance = 1.1m;
+            var accountNumber = 1;
+
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, accountNumber.ToString())
+            }));
+
+            var mockUserDAL = new Mock<IUserDAL>();
+            var mockTransactionDAL = new Mock<ITransactionDAL>();
+
+            mockUserDAL.Setup(x => x.GetUserData(It.IsAny<long>())).Returns(new User
+            {
+                LoginName = name
+            });
+
+            mockTransactionDAL.Setup(x => x.GetBalance(It.IsAny<long>())).Returns(balance);
+
+            var _homeController = new HomeController(mockUserDAL.Object, mockTransactionDAL.Object);
+            _homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var result = _homeController.Withdraw();
+
+            var r = result as ViewResult;
+
+            Assert.AreEqual(r.ViewData["LoginName"], name);
+            Assert.AreEqual(r.ViewData["AccountNumber"].ToString(), accountNumber.ToString());
+            Assert.AreEqual(r.ViewData["Balance"], balance);
+        }
+
+        [TestMethod]
+        public void TransferNoValues()
+        {
+            var name = "testuser1";
+            var balance = 1.1m;
+            var accountNumber = 1;
+
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, accountNumber.ToString())
+            }));
+
+            var mockUserDAL = new Mock<IUserDAL>();
+            var mockTransactionDAL = new Mock<ITransactionDAL>();
+
+            mockUserDAL.Setup(x => x.GetUserData(It.IsAny<long>())).Returns(new User
+            {
+                LoginName = name
+            });
+
+            mockTransactionDAL.Setup(x => x.GetBalance(It.IsAny<long>())).Returns(balance);
+
+            var _homeController = new HomeController(mockUserDAL.Object, mockTransactionDAL.Object);
+            _homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var result = _homeController.Transfer();
+
+            var r = result as ViewResult;
+
+            Assert.AreEqual(r.ViewData["LoginName"], name);
+            Assert.AreEqual(r.ViewData["AccountNumber"].ToString(), accountNumber.ToString());
+            Assert.AreEqual(r.ViewData["Balance"], balance);
+        }
+
+        [TestMethod]
+        public void DepositWithValues()
+        {
+            var name = "testuser1";
+            var balance = 1.1m;
+            var accountNumber = 1;
+
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, accountNumber.ToString())
+            }));
+
+            var mockUserDAL = new Mock<IUserDAL>();
+            var mockTransactionDAL = new Mock<ITransactionDAL>();
+
+            mockUserDAL.Setup(x => x.GetUserData(It.IsAny<long>())).Returns(new User
+            {
+                LoginName = name
+            });
+
+            mockTransactionDAL.Setup(x => x.GetBalance(It.IsAny<long>())).Returns(balance);
+
+            var _homeController = new HomeController(mockUserDAL.Object, mockTransactionDAL.Object);
+            _homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var result = _homeController.Deposit(new Transaction
+            {
+                Amount = 1.1m,
+                AccountNumber = accountNumber
+            });
+
+            var r = result as ViewResult;
+
+            Assert.AreEqual(r.ViewData["LoginName"], name);
+            Assert.AreEqual(r.ViewData["AccountNumber"].ToString(), accountNumber.ToString());
+            Assert.AreEqual(r.ViewData["Balance"], balance);
+        }
+
+        [TestMethod]
+        public void WithdrawWithValues()
+        {
+            var name = "testuser1";
+            var balance = 1.1m;
+            var accountNumber = 1;
+
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, accountNumber.ToString())
+            }));
+
+            var mockUserDAL = new Mock<IUserDAL>();
+            var mockTransactionDAL = new Mock<ITransactionDAL>();
+
+            mockUserDAL.Setup(x => x.GetUserData(It.IsAny<long>())).Returns(new User
+            {
+                LoginName = name
+            });
+
+            mockTransactionDAL.Setup(x => x.GetBalance(It.IsAny<long>())).Returns(balance);
+
+            var _homeController = new HomeController(mockUserDAL.Object, mockTransactionDAL.Object);
+            _homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var result = _homeController.Withdraw(new Transaction
+            {
+                Amount = 1.1m,
+                AccountNumber = accountNumber
+            });
+
+            var r = result as ViewResult;
+
+            Assert.AreEqual(r.ViewData["LoginName"], name);
+            Assert.AreEqual(r.ViewData["AccountNumber"].ToString(), accountNumber.ToString());
+            Assert.AreEqual(r.ViewData["Balance"], balance);
+        }
+
+        [TestMethod]
+        public void TransferWithValues()
+        {
+            var name = "testuser1";
+            var balance = 1.1m;
+            var accountNumber = 1;
+
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                 new Claim(ClaimTypes.Name, accountNumber.ToString())
+            }));
+
+            var mockUserDAL = new Mock<IUserDAL>();
+            var mockTransactionDAL = new Mock<ITransactionDAL>();
+
+            mockUserDAL.Setup(x => x.GetUserData(It.IsAny<long>())).Returns(new User
+            {
+                LoginName = name
+            });
+
+            mockTransactionDAL.Setup(x => x.GetBalance(It.IsAny<long>())).Returns(balance);
+
+            var _homeController = new HomeController(mockUserDAL.Object, mockTransactionDAL.Object);
+            _homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
+
+            var result = _homeController.Transfer(new Transaction
+            {
+                Amount = 1.1m,
+                AccountNumber = accountNumber
+            });
+
+            var r = result as ViewResult;
+
+            Assert.AreEqual(r.ViewData["LoginName"], name);
+            Assert.AreEqual(r.ViewData["AccountNumber"].ToString(), accountNumber.ToString());
+            Assert.AreEqual(r.ViewData["Balance"], balance);
+        }
     }
 }
